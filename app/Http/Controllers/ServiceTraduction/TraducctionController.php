a60ba1549cece24cbe407e2b6945531dc6ff2d89
Daniel Felipe Garcia Forero <daniel.garcia@decameron.com>
2018-11-02 17:58:23 -0500

    lumen back deca

---
+++ app/Http/Controllers/ServiceTraduction/TraducctionController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers\ServiceTraduction;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use DB;
+
+class TraducctionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+         return "pruebas";
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+    public function JsonTraducciones(Request $request)
+    {
+        if($request->isJson()){
+
+            $varConsulta= DB::table("dblink.vis_lang_object")->select('object_name','translation')->where('lang_id', $request['idioma'])->get();
+            $arrayData = array();
+            foreach($varConsulta as $row){
+                $arrayData += array($row->object_name => $row->translation);
+            }
+            return response()->json($arrayData);
+        }
+        return response()->json(['error'=>'fail']);
+        
+        
+    }
+}
